9252d52836b0c4d5903f1176ecaae9d783a0a7ee
pertiol <pertiol@yandex.ua>
2015-07-14 18:04:55 +0300

    JavaDoc added

--- ArgRunner.java
+++ ArgRunner.java
@@ -1,16 +1,24 @@
 /**
-	Calculate in command line
-*/
+ * Calculate in command line
+ * Use: java ArgRunner firstNumber operation secondNumber
+ * @author pertiol
+ * @date 14/07/2015
+ * @version 1.0
+ */
 
 public class ArgRunner {
 	
 	public static void main(String[] args) {
-		
+		/**
+		 * Initialize numbers and operation type
+		 */
 		double first = Double.valueOf(args[0]);
 		String operation = args[1];
 		double second = Double.valueOf(args[2]);
 		Calculator calc = new Calculator();
-		
+		/**
+		 * Check operation for performing, if incorrect - show message to user
+		 */
 		if(operation.equals("+"))
 			calc.add(first, second);
 		else if(operation.equals("*"))
@@ -22,6 +30,9 @@ public class ArgRunner {
 		else {
 			System.out.println("Incorrect operation.");
 		}
+		/**
+		 * Show result
+		 */
 		System.out.println("Result is " + calc.getResult());
 	}
 	
--- InteractRunner.java
+++ InteractRunner.java
@@ -1,6 +1,9 @@
 /**
-	Calculate in interaction mode
-*/
+ * Calculate in interaction mode
+ * @author pertiol
+ * @date 14.07.2015
+ * @version 1.1
+ */
 
 import java.util.Scanner;
 import java.util.*;
@@ -8,16 +11,35 @@ import java.util.*;
 public class InteractRunner {
 	
 	public static void main(String[] args) {
-		
+		/**
+		 * Create new instance of Scanner class
+		 */
 		Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+		/**
+		 * Create new instance of Calculator class
+		 */
 		Calculator calc = new Calculator();
+		/**
+		 * Strings for storing user inputs
+		 */
 		String exit = "no";
 		String enter = "";
+		/**
+		 * Variable for check whether need to save result
+		 */
 		boolean saveResult = false;
 		double first = 0, second = 0;
 		try{
+			/**
+			 * Loop performing while user won't input "yes"
+			 */
 			while(!exit.equals("yes")) {
 				System.out.println("Enter the first number: ");
+				/**
+				 * Check whether user wanted to save result <br>
+				 * if no - take next number for scan <br>
+				 * if yes - get result
+				 */
 				if(!saveResult)
 					first = scan.nextDouble();
 				else {
@@ -39,10 +61,16 @@ public class InteractRunner {
 					else
 						second = Double.valueOf(enter);
 				}
+				/**
+				 * Prevent dividing by zero
+				 */				
 				if (second == 0) {
 					System.out.println("Error. Dividing by zero.");
 					continue;
 				}
+				/**
+				 * Check operation for performing, if incorrect - continue loop from beginning
+				 */
 				if(operation.equals("+"))
 					calc.add(first, second);
 				else if(operation.equals("*"))
@@ -58,18 +86,28 @@ public class InteractRunner {
 					continue;
 				}
 				System.out.println("Result is " + calc.getResult());
+				/**
+				 * Ask if user want to quit and store answer to "exit"
+				 */
 				System.out.println("Do you want to quit? (\"yes\" or \"no\")");
 				exit = scan.next();
 				if (exit.equals("yes"))
 					break;
+				/**
+				 * Ask if user want to save, if yes - change saveResult to true
+				 */
 				System.out.println("Do you want to save result? (\"yes\" or \"no\")");
 				if (scan.next().equals("yes")) {
 					saveResult = true;
 					System.out.println("Type \"MR\" instead needed operand to recall result from memory.");
 				} else {
+					saveResult = false;
 					calc.clearResult();
 				}
 			}
+		/**
+		 * Catch possible exceptions
+		 */
 		} catch (NumberFormatException | InputMismatchException e) {
 			System.out.println("Error occurred.");
 		}
